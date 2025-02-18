5b92f0572def23cb8cb875145cf2b365e74e373f
noey <yoneee909@gmail.com>
2025-02-19 04:12:14 +0900

    페이지네이션 최적화

--- refactoring/src/components/boards-list/pagination/index.tsx
+++ refactoring/src/components/boards-list/pagination/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 import { usePagination } from "./hook";
 import { IPaginationProps } from "./types";
 import style from "./styles.module.css";
@@ -8,6 +8,22 @@ import style from "./styles.module.css";
 export default function PaginationPage(props: IPaginationProps) {
   const { startPage, onClickPage, onClickPrevPage, onClickNextPage } =
     usePagination(props);
+
+  const pageButtons = useMemo(() => {
+    return Array.from({ length: 5 }, (_, index) =>
+      index + startPage <= props.lastPage ? (
+        <button
+          key={index + startPage}
+          id={String(index + startPage)}
+          onClick={onClickPage}
+          className={style.indexButton}
+        >
+          {index + startPage}
+        </button>
+      ) : null
+    );
+  }, [startPage, props.lastPage, onClickPage]);
+
   return (
     <div className={style.paginationLayout}>
       {/* 첫 페이지 그룹에서는 이전 버튼 비활성화 */}
@@ -16,19 +32,7 @@ export default function PaginationPage(props: IPaginationProps) {
         className={style.prevButton}
         disabled={startPage === 1}
       >{`<`}</button>
-      {new Array(5).fill("").map(
-        (_, index) =>
-          index + startPage <= props.lastPage && (
-            <button
-              key={index + startPage}
-              id={String(index + startPage)}
-              onClick={onClickPage}
-              className={style.indexButton}
-            >
-              {index + startPage}
-            </button>
-          )
-      )}
+      {pageButtons}
       {/* 마지막 페이지 그룹에서 다음 버튼 비활성화 */}
       <button
         onClick={onClickNextPage}
